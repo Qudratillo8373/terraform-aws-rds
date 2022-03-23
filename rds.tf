0c45f7356c5b1fb115d9b1181dcd6e4c99ab9d98
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-23 17:25:42 -0500

    change name

--- rds.tf
+++ rds.tf
@@ -2,9 +2,9 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 50
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  parameter_group_name = "default.mysql5.7"
+  instance_class       = "db.t3.large"
+  name                 = "Kudratillo-db"
+  parameter_group_name = "GRmysql5.7"
   skip_final_snapshot  = true
   auto_minor_version_upgrade = true
 
