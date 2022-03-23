a5284e05a3782165fcad99b9efb5be18361f7abd
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-23 17:33:39 -0500

    create module

---
+++ Module/module.tf
@@ -0,0 +1,15 @@
+module aws_db_instance_default {
+    source = "../"
+    allocated_storage    = 50
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.large"
+  name                 = "Kudratillo-db"
+  parameter_group_name = "GRmysql5.7"
+  skip_final_snapshot  = true
+  auto_minor_version_upgrade = true
+
+    tags = {
+        Name = "Dev"
+    }
+}
